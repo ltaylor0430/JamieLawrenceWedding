ed4d285bc48c34cf789c49e4f8052308525535e0
Lawrence Taylor <ltaylor0430@yahoo.com>
2013-04-16 19:51:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamieLawrenceWedding::Application.config.secret_token = 'a3be9b16af182a1378ce5066046cf38494a67f598d83c842baf6e5b5b7335034e7a774e9b97cf9071571e654975f1cb6991364ee490bab94aca6c0f6e6bc999a'
