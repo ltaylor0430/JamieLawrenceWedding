d7c35fb39e71bb999f30ca91fb0f0877e658b259
Lawrence Taylor <ltaylor0430@yahoo.com>
2013-05-30 23:01:53 -0400

    I haven't been doing this right but this project doesn't require much rails TDD

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function () {
+ 
+$('#contentContainer').addClass(' animated bounceInDown');
+ 
+});
+ 
+  $(function() {
+  var active;
+   var links = $('a.link').click(function(event) {
+   	   event.preventDefault();
+
+    console.debug("removing class active.");
+   console.debug(event);
+   var hash = event.target.hash;
+   console.debug(hash);
+   	$("#headerList > li a").each(function(i,k) {
+   		
+  	 if ($(this).parent().hasClass("active"))
+  	 	active = k.hash;
+  	 if (hash == (k.hash))
+   	 {
+   	 	
+   	 	$(this).parent().addClass("active");
+   	 }
+   	 else
+   	 {
+   	 	$(this).parent().removeClass("active");
+   	 }
+ 
+
+   	});
+   	//fade out current selection and show the new one
+   	$("#contentContainer").addClass("bounceOutUp");
+   	$(active).toggleClass("hide");
+     	
+   	//remove 
+   	var wait = window.setTimeout( function(){
+			$('#contentContainer').removeClass("bounceOutUp");
+			$('#contentContainer').addClass('bounceInDown');
+			$(hash).toggleClass("hide");
+			if (hash == "#album")
+   			{
+	   	    
+				// Can also be used with $(document).ready()
+				 console.debug("album tag");
+				$('.flexslider').flexslider({
+				    animation: "slide"
+				});
+	 
+	    
+   			}
+		   else if ((hash == "#directions" )&& (directionsService == undefined))
+		   {
+			   	loadScript();
+			   	console.debug("google script loaded");
+		   }
+			},
+			400
+		);
+    
+   
+	
+   });
+  });
